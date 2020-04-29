7fc673a09ceada531847899c4eeeaa6bc88e4286
kentonhuang <huangkenton@gmail.com>
2020-04-29 13:56:54 -0700

    login/logout auth

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -43,6 +43,48 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 })
 
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+  user.tokens = user.tokens.concat({ token })
+  user.save()
+
+  return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if(!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if(!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
 // Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this
--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -11,6 +12,29 @@ router.post('/users', async (req, res) => {
     res.status(201).send(user)
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (e) {
+    res.status(400).send()
+  }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((tokens) => {
+      return tokens.token !== req.token
+    }) 
+    await req.user.save()
+
+    res.send()
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
